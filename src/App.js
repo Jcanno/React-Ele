862c834cafea81f6b59aaff98ae2ef624873d096
金志强 <641283231@qq.com>
2019-04-29 13:06:30 +0800

    re-init project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button } from 'antd';
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button>按一下</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
