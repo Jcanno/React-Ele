0d9c7443c2c20ff9ed943e650ec7d52b81a9c260
金志强 <641283231@qq.com>
2019-04-29 14:06:45 +0800

    modify config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -356,6 +356,13 @@ module.exports = function(webpackEnv) {
                       },
                     },
                   ],
+                  [
+                    require.resolve('babel-plugin-import'),
+                    {
+                      "libraryName": "antd",
+                      "style": "css"
+                    }
+                  ]
                 ],
                 // This is a feature of `babel-loader` for webpack (not Babel itself).
                 // It enables caching results in ./node_modules/.cache/babel-loader/
--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.less';
+import { Button } from 'antd';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button type="primary">按一下</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
