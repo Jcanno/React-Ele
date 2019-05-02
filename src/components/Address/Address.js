1aa351960b0ab8500ed107763973e866473bbe14
金志强 <641283231@qq.com>
2019-05-02 13:15:14 +0800

    address form end

--- src/components/Address/Address.js
+++ src/components/Address/Address.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react'
 import SiderBar from '../Layout/SiderBar/SiderBar'
 import './Address.less'
-import { Card, Row, Col, Icon } from 'antd';
+import { Card, Row, Col, Icon, Modal, Form, Input, Radio } from 'antd';
 
 export default class Address extends Component {
+
+  state = {
+    visible: false
+  }
+
+  onEdit = () => {
+    this.setState({
+      visible: true,
+    })
+  }
+
+  handleCancel = () => {
+    this.setState({ visible: false });
+  }
+
+  handleCreate = () => {
+    const form = this.formRef.props.form;
+    form.validateFields((err, values) => {
+      if (err) {
+        return;
+      }
+
+      console.log('Received values of form: ', values);
+      form.resetFields();
+      this.setState({ visible: false });
+    });
+  }
+
+  saveFormRef = (formRef) => {
+    this.formRef = formRef;
+  }
+
+
+
+
   render() {
     return (
       <div className="address">
@@ -23,14 +58,14 @@ export default class Address extends Component {
                     <span className="address-sex">
                       先生
                     </span>
-                    <div className="address-operate">
-                      <span className="address-edit">
+                    <span className="address-operate">
+                      <span className="address-edit" onClick={this.onEdit}>
                         修改
                       </span>
                       <span>
                         删除
                       </span>
-                    </div>
+                    </span>
                   </p>
                   <span className="address-place">
                     杭州市西湖区中大银座A座2222室
@@ -46,11 +81,67 @@ export default class Address extends Component {
                 <Icon type="plus" />
                 添加新地址
               </Card>
-            </Col>
-            
+            </Col>            
           </Row>
         </Card>
+        <AddOrEditAddressForm 
+          wrappedComponentRef={this.saveFormRef}
+          visible={this.state.visible}
+          onCancel={this.handleCancel}
+          onOk={this.handleCreate}
+        />
       </div>
     )
   }
 }
+
+
+const AddOrEditAddressForm = Form.create({ name: 'form_in_modal' })(
+  // eslint-disable-next-line
+  class extends React.Component {
+    render() {
+      const {
+        visible, onCancel, onOk, form,
+      } = this.props;
+      const { getFieldDecorator } = form;
+      return (
+        <Modal
+          title="编辑地址"
+          visible={visible}
+          onOk={onOk}
+          onCancel={onCancel}
+          okText="保存"
+          cancelText="取消"
+        >
+          <Form layout="vertical">
+            <Form.Item label="姓名">
+              {getFieldDecorator('name', {
+                rules: [{ required: true, message: '请输入姓名!' }],
+              })(
+                <Input placeholder="姓名" />
+              )}
+            </Form.Item>
+            <Form.Item label="性别">
+              {getFieldDecorator('sex', )(
+                <Radio.Group>
+                  <Radio value="male">先生</Radio>
+                  <Radio value="female">女士</Radio>
+                </Radio.Group>
+              )}
+            </Form.Item>
+            <Form.Item label="详细位置">
+              {getFieldDecorator('address', {
+                rules: [{ required: true, message: '请输入详细位置!' }],
+              })(<Input placeholder="详细位置" />)}
+            </Form.Item>
+            <Form.Item label="手机号">
+              {getFieldDecorator('phone', {
+                rules: [{ required: true, message: '请输入手机号!' }],
+              })(<Input placeholder="手机号" />)}
+            </Form.Item>
+          </Form>
+        </Modal>
+      );
+    }
+  }
+);
