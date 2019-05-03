b82b13356cb2789619aef3f2633fbc79961c654f
金志强 <641283231@qq.com>
2019-05-03 18:18:51 +0800

    detail item end

--- src/components/StoreDetail/StoreDetail.js
+++ src/components/StoreDetail/StoreDetail.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import './StoreDetail.less'
-import { Avatar, Typography, Rate } from 'antd'
+import { Avatar, Typography, Rate, Row, Col, Button, Icon } from 'antd'
 import store from '@/assets/store.jpg'
 
 export default class StoreDetail extends Component {
   render() {
 
-    const { Title } = Typography;
+    const { Title, Text } = Typography;
 
     return (
       <div>
@@ -28,6 +28,7 @@ export default class StoreDetail extends Component {
                 className="detail-rate"
                 defaultValue={4.5}
                 allowHalf
+                disabled
               />
             </div>
           </div>
@@ -45,8 +46,150 @@ export default class StoreDetail extends Component {
               <strong>30分钟</strong>
             </div>
           </div>
-            
-          
+        </div>
+        <div className="detail-goods">
+
+          <Row gutter={10}>
+            <Col span={12}>
+              <div className="detail-item">
+                <Avatar 
+                  src={store}
+                  size={100}
+                  shape="square"
+                  className="detail-avatar"
+                />
+                <div className="detail-goodbox">
+                  <Text className="detail-goodname">
+                    咖喱鸡烩饭+水果沙拉
+                  </Text>
+                  <Text className="detail-gooddesc">
+                    主要原料: 墨鱼, 蛤蜊主要原料: 墨鱼, 蛤蜊主要原料: 墨鱼, 蛤蜊
+                  </Text>
+                  <Rate 
+                    className="detail-goodrate"
+                    defaultValue={4.5}
+                    allowHalf
+                    disabled
+                  />
+                  <Text className="detail-goodcount">
+                    月售2份
+                  </Text>
+                  <Text className="detail-goodprice">
+                    ￥66
+                    <Button type="primary" shape="round" size="small" style={{float: "right"}}>
+                      加入购物车
+                    </Button>
+                  </Text>
+                  
+                </div>
+              </div>
+            </Col>
+
+            <Col span={12}>
+              <div className="detail-item">
+                <Avatar 
+                  src={store}
+                  size={100}
+                  shape="square"
+                  className="detail-avatar"
+                />
+                <div className="detail-goodbox">
+                  <Text className="detail-goodname">
+                    咖喱鸡烩饭+水果沙拉
+                  </Text>
+                  <Text className="detail-gooddesc">
+                    主要原料: 墨鱼, 蛤蜊主要原料: 墨鱼, 蛤蜊主要原料: 墨鱼, 蛤蜊
+                  </Text>
+                  <Rate 
+                    className="detail-goodrate"
+                    defaultValue={4.5}
+                    allowHalf
+                    disabled
+                  />
+                  <Text className="detail-goodcount">
+                    月售2份
+                  </Text>
+                  <Text className="detail-goodprice">
+                    ￥66
+                    <Button type="primary" shape="round" size="small" style={{float: "right"}}>
+                      加入购物车
+                    </Button>
+                  </Text>
+                  
+                </div>
+              </div>
+            </Col>
+
+            <Col span={12}>
+              <div className="detail-item">
+                <Avatar 
+                  src={store}
+                  size={100}
+                  shape="square"
+                  className="detail-avatar"
+                />
+                <div className="detail-goodbox">
+                  <Text className="detail-goodname">
+                    咖喱鸡烩饭+水果沙拉
+                  </Text>
+                  <Text className="detail-gooddesc">
+                    主要原料: 墨鱼, 蛤蜊主要原料: 墨鱼, 蛤蜊主要原料: 墨鱼, 蛤蜊
+                  </Text>
+                  <Rate 
+                    className="detail-goodrate"
+                    defaultValue={4.5}
+                    allowHalf
+                    disabled
+                  />
+                  <Text className="detail-goodcount">
+                    月售2份
+                  </Text>
+                  <Text className="detail-goodprice">
+                    ￥66
+                    <Button type="primary" shape="round" size="small" style={{float: "right"}}>
+                      加入购物车
+                    </Button>
+                  </Text>
+                  
+                </div>
+              </div>
+            </Col>
+
+            <Col span={12}>
+              <div className="detail-item">
+                <Avatar 
+                  src={store}
+                  size={100}
+                  shape="square"
+                  className="detail-avatar"
+                />
+                <div className="detail-goodbox">
+                  <Text className="detail-goodname">
+                    咖喱鸡烩饭+水果沙拉
+                  </Text>
+                  <Text className="detail-gooddesc">
+                    主要原料: 墨鱼, 蛤蜊主要原料: 墨鱼, 蛤蜊主要原料: 墨鱼, 蛤蜊
+                  </Text>
+                  <Rate 
+                    className="detail-goodrate"
+                    defaultValue={4.5}
+                    allowHalf
+                    disabled
+                  />
+                  <Text className="detail-goodcount">
+                    月售2份
+                  </Text>
+                  <Text className="detail-goodprice">
+                    ￥66
+                    <Button type="primary" shape="round" size="small" style={{float: "right"}}>
+                      加入购物车
+                    </Button>
+                  </Text>
+                  
+                </div>
+              </div>
+            </Col>
+          </Row>
         </div>
       </div>
     )
